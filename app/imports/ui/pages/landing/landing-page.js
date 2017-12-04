b16f58ad992fc282f149494613e9c017b87bcadd
lihongman <herman_hmli@yahoo.com>
2017-12-03 19:14:40 -1000

    Finished Spotlight organization

---
+++ app/imports/ui/pages/landing/landing-page.js
@@ -0,0 +1,24 @@
+import { Template } from 'meteor/templating';
+import { Clubs } from '/imports/api/clubprofile/ClubProfileCollection';
+import { _ } from 'meteor/underscore';
+
+Template.Landing_Page.onCreated(function onCreated() {
+  this.subscribe(Clubs.getPublicationName());
+});
+
+Template.Landing_Page.helpers({
+  /**
+   * Returns a cursor to profiles, sorted by last name.
+   */
+  clubs() {
+    var club = Clubs.find({}, {fields: {bio: 1}});
+    club = _.sample(Object.values(club.collection._docs._map));
+    console.log(club)
+    while(club.pictures.length < 1) {
+      club = Clubs.find({}, {fields: {bio: 1}});
+      club = _.sample(Object.values(club.collection._docs._map));
+      console.log(club);
+    }
+    return [club];
+  }
+});
