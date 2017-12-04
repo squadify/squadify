888625a51fa255a37dc5a400431f1ee66cd345b9
yongkim93 <31455618+yongkim93@users.noreply.github.com>
2017-12-04 12:09:00 -1000

    slideshow
    functional fix and moved it to component.

---
+++ app/imports/ui/components/slideshow/index.js
@@ -0,0 +1,3 @@
+import './slideshow.html';
+import './slideshow.css';
+import './slideshow.js';
--- app/imports/ui/pages/club/club-page.js
+++ app/imports/ui/pages/club/club-page.js
@@ -2,7 +2,6 @@ import { Template } from 'meteor/templating';
 import { $ } from 'meteor/jquery';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { _ } from 'meteor/underscore';
 
 import './club-page.html';
 
@@ -14,30 +13,6 @@ Template.Club_Page.helpers({
 });
 
 Template.Club_Page.events({
-  'click .next-to-right'(event, instance) {
-    // console.log('clicked right button');
-    // console.log(Template.instance().slides.get());
-    // Template.instance().currentIndex.get()++;
-    const slides = document.getElementsByClassName('myslides');
-    let index = instance.currentIndex.get();
-    let i;
-    // console.log(instance.currentIndex.get());
-    $(slides[index]).transition('fade right');
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-    }
-    index = (index + 1) % slides.length;
-    console.log(index);
-    slides[index].style.display = 'block';
-    $(slides[index]).transition('fade left');
-    instance.currentIndex.set(index);
-    // dots[i].className = dots[i].className.replace(" active", "");
-  },
-  'click .next-to-left': function () {
-    console.log('clicked right button');
-    $('.dog.image').transition('fade left');
-    $('.dog.image').transition('fade right');
-  },
   'click .huge.green.button': function () {
     console.log('apply');
     FlowRouter.route('/');
