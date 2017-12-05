263ea6a8c12efb5e3f9a2480031e9a77e13cde0d
yongkim93 <31455618+yongkim93@users.noreply.github.com>
2017-12-04 15:54:33 -1000

    apply modal - unfinished

--- app/imports/ui/components/slideshow/slideshow.js
+++ app/imports/ui/components/slideshow/slideshow.js
@@ -14,23 +14,25 @@ Template.Slideshow.helpers({
 Template.Slideshow.events({
   'click .next-to-right'(event, instance) {
     // console.log('clicked right button');
+    event.preventDefault();
     const slides = document.getElementsByClassName('myslide');
     const box = document.getElementsByClassName('slidebox');
     const currentIndex = instance.currentIndex.get();
     const nextIndex = (currentIndex > 0) ? (currentIndex - 1) : slides.length - 1;
     // console.log(nextIndex);
     $(box)
-        .transition('fade left', '500ms', function () {
+        .transition('fade left', '300ms', function () {
           slides[currentIndex].style.display = 'none';
           slides[nextIndex].style.display = 'block';
         })
-        .transition('fade right', '500ms', function () {
+        .transition('fade right', '300ms', function () {
         })
     ;
     instance.currentIndex.set(nextIndex);
   },
   'click .next-to-left'(event, instance) {
     // console.log('clicked left button');
+    event.preventDefault();
     const slides = document.getElementsByClassName('myslide');
     const box = document.getElementsByClassName('slidebox');
     const currentIndex = instance.currentIndex.get();
--- app/imports/ui/pages/club/club-page.js
+++ app/imports/ui/pages/club/club-page.js
@@ -1,20 +1,113 @@
 import { Template } from 'meteor/templating';
-import { $ } from 'meteor/jquery';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { _ } from 'meteor/underscore';
+import { $ } from 'meteor/jquery';
 
-import './club-page.html';
+const displaySuccessMessage = 'displaySuccessMessage';
+const displayErrorMessages = 'displayErrorMessages';
 
 Template.Club_Page.onCreated(function onCreated() {
-  this.currentIndex = new ReactiveVar(0);
+  // this.subscribe(Interests.getPublicationName());
+  // this.subscribe(Profiles.getPublicationName());
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(displaySuccessMessage, false);
+  this.messageFlags.set(displayErrorMessages, false);
+  // this.context = Profiles.getSchema().namedContext('Profile_Page');
 });
 
 Template.Club_Page.helpers({
+  successClass() {
+    return Template.instance().messageFlags.get(displaySuccessMessage) ? 'success' : '';
+  },
+  displaySuccessMessage() {
+    return Template.instance().messageFlags.get(displaySuccessMessage);
+  },
+  errorClass() {
+    return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
+  },
+  /*
+  profile() {
+    return Profiles.findDoc(FlowRouter.getParam('username'));
+  },
+  interests() {
+    const profile = Profiles.findDoc(FlowRouter.getParam('username'));
+    const selectedInterests = profile.interests;
+    return profile && _.map(Interests.findAll(),
+        function makeInterestObject(interest) {
+          return { label: interest.name, selected: _.contains(selectedInterests, interest.name) };
+        });
+  },
+  */
 });
 
 Template.Club_Page.events({
   'click .huge.green.button': function () {
-    console.log('apply');
-    FlowRouter.route('/');
+    // console.log('apply');
+    // FlowRouter.route('/');
+    $('.ui.modal')
+        .modal({
+          blurring: true,
+        })
+        .modal({
+          closable: true,
+          onDeny: function () {
+          },
+          onApprove: function () {
+            /*
+            const name = document.registration('Name').value;
+            const email = document.registration('Email').value;
+            const text = document.registration('TextArea').value;
+            console.log(name);
+            console.log(email);
+            console.log(text);
+            */
+          },
+        })
+        .modal('show')
+    ;
   },
+  /*
+  'submit .actions'(event, instance) {
+    event.preventDefault();
+    console.log('submit');
+    console.log(event);
+    const name = event.target.Name.value;
+    const email = event.target.Email.value;
+    const text = event.target.TextArea.value;
+
+    console.log(name);
+    console.log(email);
+    console.log(text);
+
+    const title = event.target.Title.value;
+    const username = FlowRouter.getParam('username'); // schema requires username.
+    const picture = event.target.Picture.value;
+    const github = event.target.Github.value;
+    const facebook = event.target.Facebook.value;
+    const instagram = event.target.Instagram.value;
+    const bio = event.target.Bio.value;
+    const selectedInterests = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
+    const interests = _.map(selectedInterests, (option) => option.value);
+
+    const updatedProfileData = { firstName, lastName, title, picture, github, facebook, instagram, bio, interests,
+      username };
+    // Clear out any old validation errors.
+    instance.context.reset();
+    // Invoke clean so that updatedProfileData reflects what will be inserted.
+    const cleanData = Profiles.getSchema().clean(updatedProfileData);
+    // Determine validity.
+    instance.context.validate(cleanData);
+
+    if (instance.context.isValid()) {
+      const docID = Profiles.findDoc(FlowRouter.getParam('username'))._id;
+      const id = Profiles.update(docID, { $set: cleanData });
+      instance.messageFlags.set(displaySuccessMessage, id);
+      instance.messageFlags.set(displayErrorMessages, false);
+    } else {
+      instance.messageFlags.set(displaySuccessMessage, false);
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
+  },
+  */
 });
