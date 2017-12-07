a01f121e0d9aa68492ed694a7bc79820f87507f5
Bryant Sanchez <bryantsa@hawaii.edu>
2017-12-07 10:52:08 -1000

    isuue-18

--- app/imports/ui/pages/list/list-page.js
+++ app/imports/ui/pages/list/list-page.js
@@ -2,10 +2,13 @@ import { Template } from 'meteor/templating';
 import { Clubs } from '/imports/api/clubprofile/ClubProfileCollection';
 import { Tags } from '/imports/api/tags/InterestsCollection';
 import { _ } from 'meteor/underscore';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 Template.List_Page.onCreated(function onCreated() {
   this.subscribe(Clubs.getPublicationName());
   this.subscribe(Tags.getPublicationName());
+  this.state = new ReactiveDict();
+  this.state.set('tag', 'none');
 });
 let clublist = Clubs.find({}, { sort: { name: 1 } });
 
@@ -15,33 +18,27 @@ Template.List_Page.helpers({
    */
 
   clubs() {
-    //filter();
+    let clublist = Clubs.findAll();
+    const instance = Template.instance();
+
+    if (instance.state.get('tag') !== 'none') {
+      let tag = instance.state.get('tag');
+      clublist = _.filter(clublist, function (club) {
+        return _.contains(club.tags, tag);
+      });
+      console.log(clublist);
+      return clublist;
+    }
     return clublist;
   },
   tags() {
     return Tags.find({}, { sort: { name: 1 } });
   },
-  filter() {
-    clublist = Clubs.find({}, { sort: { name: 1 } });
-    clublist = Object.values(clublist.collection._docs._map);
-    clublist = _.filter(clublist, function (club) {
-      return _.find(club.tags, function (tag) {
-        return tag == 'Arts'
-      });
-    });
-    return clublist;
-  }
-  });
+});
 
 Template.List_Page.events({
-  '.click .tag'(event) {
-    clublist = Clubs.find({}, { sort: { name: 1 } });
-    clublist = Object.values(clublist.collection._docs._map);
-    clublist = _.filter(clublist, function (club) {
-      return _.find(club.tags, function (tag) {
-        return tag == event
-      });
-    });
-    return clublist;
-  }
-})
+  'click .tag'(event, instance) {
+    instance.state.set('tag', $(event.target).text());
+  },
+});
+
