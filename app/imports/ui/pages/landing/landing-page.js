66ec355ed0009cbaecca4af0c2279e7a60e66a3d
lihongman <herman_hmli@yahoo.com>
2017-12-13 13:43:16 -1000

    UI Update

--- app/imports/ui/pages/landing/landing-page.js
+++ app/imports/ui/pages/landing/landing-page.js
@@ -6,12 +6,14 @@ Template.Landing_Page.onCreated(function onCreated() {
   this.subscribe(Clubs.getPublicationName());
 });
 
+let club;
+
 Template.Landing_Page.helpers({
   /**
    * Returns a cursor to profiles, sorted by last name.
    */
   clubs() {
-    let club = Clubs.find({}, {fields: {bio: 1}});
+    club = Clubs.find({}, {fields: {bio: 1}});
     club = _.sample(Object.values(club.collection._docs._map));
     while(club.pictures.length < 1) {
       club = Clubs.find({}, {fields: {bio: 1}});
@@ -19,4 +21,10 @@ Template.Landing_Page.helpers({
     }
     return [club];
   }
+});
+
+Template.Landing_Page.events({
+  'click .club.button'(event, instance) {
+    location.href = FlowRouter.path("/club-page/:_id", { _id: club._id });
+  },
 });
